554617227b3793d69f4b32ff1134cbfa1e348a1f
Arram Rishika <135079553+arramrishika2007@users.noreply.github.com>
2026-07-04 18:28:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,24 +4,22 @@ codes[0].focus();
 
 codes.forEach((code, index) => {
 
-  code.addEventListener("input", () => {
-    code.value = code.value.slice(0, 1);
+  code.addEventListener("input", (e) => {
+    code.value = code.value.replace(/[^0-9]/g, "").slice(0, 1);
 
-    if (code.value && index < codes.length - 1) {
+    if (code.value !== "" && index < codes.length - 1) {
       codes[index + 1].focus();
     }
   });
 
   code.addEventListener("keydown", (e) => {
     if (e.key === "Backspace") {
-      code.value = "";
 
-      if (index > 0) {
-        codes[index - 1].value = "";
+      if (code.value === "" && index > 0) {
         codes[index - 1].focus();
+      } else {
+        code.value = "";
       }
-
-      e.preventDefault();
     }
   });
 
